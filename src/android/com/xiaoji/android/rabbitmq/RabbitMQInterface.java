0d5ab9ba239eab191c7562dea7cf5ad1864d7141
leon_xi <leon_xi@163.com>
2019-07-22 19:41:15 +0800

    更新启动方法

--- src/android/com/xiaoji/android/rabbitmq/RabbitMQInterface.java
+++ src/android/com/xiaoji/android/rabbitmq/RabbitMQInterface.java
@@ -2,11 +2,25 @@ package com.xiaoji.android.rabbitmq;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 
 public class RabbitMQInterface {
     public static void init(Context ctx) {
       System.out.println("RabbitMQInterface init");
         Intent intent = new Intent(ctx, RabbitMQClientService.class);
-        ctx.startService(intent);
+        //ctx.startService(intent);
+        ctx.bindService(intent, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                RabbitMQClientService.MyBinder binder =
+                        (RabbitMQClientService.MyBinder) service;
+                instance.service = binder.getService();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                // fireEvent(BackgroundMode.Event.FAILURE, "'service disconnected'");
+            }
+        }, Context.BIND_AUTO_CREATE);
     }
 }
