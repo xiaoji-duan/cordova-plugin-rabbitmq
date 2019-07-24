88e180c89cce903a2750a446aa26bd540229bb20
leon_xi <leon_xi@163.com>
2019-07-24 23:17:19 +0800

    更新判断

--- src/android/com/xiaoji/android/rabbitmq/RabbitMQClientService.java
+++ src/android/com/xiaoji/android/rabbitmq/RabbitMQClientService.java
@@ -69,7 +69,7 @@ public class RabbitMQClientService extends Service {
       String passwd = intent.getStringExtra("passwd");
 
       // 本地获取的设备ID没有加密, 从队列名中获取
-      if (queueName != null && queueName.contains(".")) {
+      if (queueName != null && queueName.contains("\\.")) {
         String[] t = queueName.split(".");
         deviceId = t[1];
       }
