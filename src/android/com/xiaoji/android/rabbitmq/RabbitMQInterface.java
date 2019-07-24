e8a1d80ba126bcb353096c8251bd2bb579de64d9
leon_xi <leon_xi@163.com>
2019-07-24 15:57:03 +0800

    更新

--- src/android/com/xiaoji/android/rabbitmq/RabbitMQInterface.java
+++ src/android/com/xiaoji/android/rabbitmq/RabbitMQInterface.java
@@ -17,23 +17,25 @@ public class RabbitMQInterface {
         Intent intent = new Intent(ctx, RabbitMQClientService.class);
 
         // 设置初始化参数
-        intent.putExtra("uid", args.getString(0));
-        intent.putExtra("deviceid", args.getString(1));
-        intent.putExtra("queueName", args.getString(2));
+        if (args.length() == 3) {
+          intent.putExtra("uid", args.getString(0));
+          intent.putExtra("deviceid", args.getString(1));
+          intent.putExtra("queueName", args.getString(2));
 
-        //ctx.startService(intent);
-        ctx.bindService(intent, new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                RabbitMQClientService.MyBinder binder =
-                        (RabbitMQClientService.MyBinder) service;
-                plugin.service = binder.getService();
-            }
+          //ctx.startService(intent);
+          ctx.bindService(intent, new ServiceConnection() {
+              @Override
+              public void onServiceConnected(ComponentName name, IBinder service) {
+                  RabbitMQClientService.MyBinder binder =
+                          (RabbitMQClientService.MyBinder) service;
+                  plugin.service = binder.getService();
+              }
 
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-                // fireEvent(BackgroundMode.Event.FAILURE, "'service disconnected'");
-            }
-        }, Context.BIND_AUTO_CREATE);
+              @Override
+              public void onServiceDisconnected(ComponentName name) {
+                  // fireEvent(BackgroundMode.Event.FAILURE, "'service disconnected'");
+              }
+          }, Context.BIND_AUTO_CREATE);
+        }
     }
 }
