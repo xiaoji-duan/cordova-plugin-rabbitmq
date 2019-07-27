29d5752156a27362694525dae2aa2684e77f1385
leon_xi <leon_xi@163.com>
2019-07-27 08:51:52 +0800

    注册或注销Receiver位置更新

--- src/android/RabbitMQPlugin.java
+++ src/android/RabbitMQPlugin.java
@@ -41,6 +41,21 @@ public class RabbitMQPlugin extends CordovaPlugin {
 
   @Override
   public void onStart() {
+  }
+
+  @Override
+  public void onStop() {
+  }
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    Log.i("RabbitMQPlugin", "RabbitMQPlugin initialize");
+
+    super.initialize(cordova, webView);
+    mContext = cordova.getActivity().getApplicationContext();
+
+    cordovaActivity = cordova.getActivity();
+
     if (mqReceiver == null) {
       mqReceiver = new RabbitMQReceiver();
 
@@ -49,83 +64,78 @@ public class RabbitMQPlugin extends CordovaPlugin {
 
       cordovaActivity.getApplicationContext().registerReceiver(mqReceiver, intentFilter);
     }
+
+    //Cordova Plugin JS command trigger
+    //RabbitMQInterface.init(mContext, instance, new JSONArray());
+
   }
 
   @Override
-  public void onStop() {
+  public void onDestroy() {
     if (mqReceiver != null) {
       cordovaActivity.getApplicationContext().unregisterReceiver(mqReceiver);
     }
   }
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-      Log.i("RabbitMQPlugin", "RabbitMQPlugin initialize");
-        super.initialize(cordova, webView);
-        mContext = cordova.getActivity().getApplicationContext();
+  /**
+   * 插件初始化
+   */
+  @Override
+  protected void pluginInitialize() {
+    Log.i("RabbitMQPlugin", "RabbitMQPlugin plugin initialize");
+  }
 
-        RabbitMQInterface.init(mContext, instance, new JSONArray());
-
-        cordovaActivity = cordova.getActivity();
+  /**
+   * 插件主入口
+   */
+  @Override
+  public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) throws JSONException {
+    Log.i("RabbitMQPlugin", "RabbitMQPlugin action " + action);
+    if (action.equals("init")) {
+      if (mContext == null) {
+        Log.i("RabbitMQPlugin", "RabbitMQPlugin mContext is null");
+      }
+      cordova.getThreadPool().execute(new Runnable() {
+          @Override
+          public void run() {
+            RabbitMQInterface.init(mContext, instance, args);
+          }
+      });
+      return true;
     }
 
-    /**
-     * 插件初始化
-     */
-    @Override
-    protected void pluginInitialize() {
-      Log.i("RabbitMQPlugin", "RabbitMQPlugin plugin initialize");
-    }
-
-    /**
-     * 插件主入口
-     */
-    @Override
-    public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) throws JSONException {
-      Log.i("RabbitMQPlugin", "RabbitMQPlugin action " + action);
-      if (action.equals("init")) {
-        if (mContext == null) {
-          Log.i("RabbitMQPlugin", "RabbitMQPlugin mContext is null");
-        }
-        cordova.getThreadPool().execute(new Runnable() {
-            @Override
-            public void run() {
-              RabbitMQInterface.init(mContext, instance, args);
-            }
-        });
+    if (action.equals("coolMethod")) {
+        String message = args.getString(0);
+        this.coolMethod(message, callbackContext);
         return true;
+    }
+
+    return false;
+  }
+
+  private void coolMethod(String message, CallbackContext callbackContext) {
+      if (message != null && message.length() > 0) {
+          callbackContext.success(message);
+      } else {
+          callbackContext.error("Expected one non-empty string argument.");
       }
+  }
 
-      if (action.equals("coolMethod")) {
-          String message = args.getString(0);
-          this.coolMethod(message, callbackContext);
-          return true;
-      }
+  static void transmitMessageReceived(JSONObject message) {
+    Log.i("RabbitMQPlugin", "received message transmit to mwxing.");
 
-      return false;
+    if (instance == null) {
+        return;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    String format = "window.plugins.RabbitMQPlugin.receivedMessageInAndroidCallback(%s);";
+    final String js = String.format(format, message.toString());
+    Log.i("RabbitMQPlugin", js);
+    cordovaActivity.runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+            instance.webView.loadUrl("javascript:" + js);
         }
-    }
-
-    static void transmitMessageReceived(JSONObject message) {
-      Log.i("RabbitMQPlugin", "received message transmit to mwxing.");
-        if (instance == null) {
-            return;
-        }
-        String format = "window.plugins.RabbitMQPlugin.receivedMessageInAndroidCallback(%s);";
-        final String js = String.format(format, message.toString());
-        Log.i("RabbitMQPlugin", js);
-        cordovaActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                instance.webView.loadUrl("javascript:" + js);
-            }
-        });
-    }
+    });
+  }
 }
